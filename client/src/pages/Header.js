0449a9c963c96fab4068d46753373f823c03cbef
Apon wuz here <frost_bird@hotmail.com>
2023-07-08 20:59:44 -0500

    created a functional logo prototype using css

---
+++ client/src/pages/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <span>V</span>ALUE
+      </h1>
+      <h2>HOME FURNISHINGS</h2>
+      <hr id="line1"></hr>
+      <hr id="line2"></hr>
+      <hr id="line3"></hr>
+      <hr id="line4"></hr>
+      <hr id="line5"></hr>
+    </header>
+  );
+}
+
+export default Header;
