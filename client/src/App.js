3130052c0225f703b2bd26b691a90d5bcfb80fdd
Aponymous <orangedragonz@gmail.com>
2023-07-14 08:31:59 -0500

    wrong source for installation, keep working

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import Header from "./pages/Header";
-// import Slideshow from "./components/slideshow";
-// import Pictures from "./components/slideshow";
-// import CarouselPage from "./components/slideshow";
+import Slideshow from "./components/slideshow";
+import Pictures from "./components/slideshow";
+import CarouselPage from "./components/slideshow";
 import "./App.css";
 // import "./src/assets";
 import Navbar from "./components/Navbar";
 import{BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+//! Navbar and Routers bugged out like mad
+//! useref && usecontext don't exist?
+
 function App() {
   return (
     <div className="App">
       <Router>
-      <Header />
-      <Navbar />
-      <Routes>
-        <Route path='/' exact />
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path='/'exact />
+        </Routes>
       </Router>
     </div>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,18 @@
-// import React, {useState} from 'react';
-import React from 'react';
+import React, {useState} from 'react';
+
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const[click, setClick ] = useState(false);
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
             TRVL <i className="fab fa-typo3"></i>
+            <div className="menu-icon">
+              <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+            </div>
           </Link>
         </div>
       </nav>
