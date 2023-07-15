f473660cce8c86931b17f65d5c073691b8b01837
Aponymous <orangedragonz@gmail.com>
2023-07-14 20:49:37 -0500

    getting my shit together

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
-import Header from "./pages/Header";
-import Slideshow from "./components/slideshow";
-import Pictures from "./components/slideshow";
-import CarouselPage from "./components/slideshow";
+// import Header from "./Header";
+// import Slideshow from "./components/slideshow";
+// import Pictures from "./components/slideshow";
+// import CarouselPage from "./components/slideshow";
 import "./App.css";
 // import "./src/assets";
 import Navbar from "./components/Navbar";
 import{BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./components/pages/Home";
 
-//! Navbar and Routers bugged out like mad
-//! useref && usecontext don't exist?
 
 function App() {
   return (
@@ -16,7 +15,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/'exact />
+          <Route path='/'exact component={Home}/>
         </Routes>
       </Router>
     </div>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,63 @@
-import React, {useState} from 'react';
-
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
   const[click, setClick ] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closedMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  };
+
+  useEffect(() => {
+    showButton()
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            TRVL <i className="fab fa-typo3"></i>
-            <div className="menu-icon">
-              <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
-            </div>
+          <Link to="/" className="navbar-logo" onClick={closedMobileMenu}>
+            VALUE <i className="fa-solid fa-house"></i>
           </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to='/' className='nav-links' onClick={closedMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/services' className='nav-links' onClick={closedMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/products' className='nav-links' onClick={closedMobileMenu}>
+                Products
+              </Link>
+            </li>
+            {/* <li className="nav-item">
+              <Link to='/sign-up' className='nav-links' onClick={closedMobileMenu}>
+                Sign Up
+              </Link>
+            </li> */}
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
     </>
