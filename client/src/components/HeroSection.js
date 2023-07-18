025f9d6a6ae9dcdcb50a76046e536386d50fa3da
Apon wuz here <frost_bird@hotmail.com>
2023-07-17 19:00:36 -0500

    added pictures

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import '../App.css'
-import { Button } from './Button'
-import './HeroSection.css';
-
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-        {/* <video src='/videos/video-2.mp4' autoPlay loop muted /> */}
-        <h1>Rentals & Sales</h1>
-        <p>What are you waiting for?</p>
-        <div className="hero-btns">
-            <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
-            {/* Second button if necessary */}
-            {/* <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>See Trailer</Button> */}
-        </div>
+    <div className="hero-container">
+      {/* <video src='/videos/video-2.mp4' autoPlay loop muted /> */}
+      <h1>Rentals & Sales</h1>
+      <p>Your Home, Your Value</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        {/* Second button if necessary */}
+        {/* <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>See Trailer</Button> */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
